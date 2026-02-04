2638914c2ac3fcae91af9dda1b20636fb354b2e1
“ikarabag1” <irmakkarabag@gmail.com>
2026-02-04 15:25:45 -0800

    fix: add path aliases for vite and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // Vite will now find index.html in the root and main.tsx in /client/src/
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client/src"),
+    },
+  },
 });
